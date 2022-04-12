10ea77a40e6957004b812e5d224ea42315d1bd77
myoung9300 <82778739+myoung9300@users.noreply.github.com>
2022-04-12 12:25:21 -0600

    Add files via upload

---
+++ instructors.js
@@ -0,0 +1,56 @@
+instructors = [
+	{
+		id: "1",
+		name: "Master Young",
+		aboutMe:
+			"I started training in the martial arts when i was 7 years old, got my black belt at 10, and started teaching when I was 13 years old. I opened my own martial arts school 2 months before I graduated high school.  Some of my favorite foods are: pizza, peanut butter & jelly sandwiches, and milk duds. I love training and teaching the martial arts to anyone that wants to learn!",
+		email: "myoung9300@yahoo.com",
+		position: "Head Instructor",
+		calendly: "https://calendly.com/masteryoung",
+	},
+	{
+		id: "2",
+		name: "Mr. Cisneros",
+		aboutMe:
+			"I am a 3rd Degree Black Belt in Tae-Kwon_Do and a Purple Belt in Brazilian Ju-Jit-Su. I love my family and kids! I really like downloading music and creating all sorts of playlists to listen to. Whatever your in the mood for, one of my playlists will get you through...",
+		email: "cisnerosnorth@comcast.net",
+		position: "Admissions Director",
+		calendly: "https://calendly.com/mr-c",
+	},
+	{
+		id: "3",
+		name: "Mr. Morrisey",
+		aboutMe:
+			"Teaching is my job, Acting is my passion! I have been training in the martial arts for over 15 years. I  am a 3rd Degree Black Belt, soon to test for my 4th. I went to collage to become  a set designer and I really love it!",
+		email: "nicmor2@gmail.com",
+		position: "Assistant Instructor",
+		calendly: "https://calendly.com/nicmor2",
+	},
+	{
+		id: "4",
+		name: "Ms. Young",
+		aboutMe:
+			"I started training in the martial arts when I was 3 years old. I am currently a 3rd Degree Black Belt and an instructor here art EMA! I have 2 younger sisters that also do martial arts. I'm currently going to college at Metro State.",
+		email: "kayleeyoung222@gmail.com",
+		position: "Assistant Instructor",
+		calendly: "",
+	},
+	{
+		id: "5",
+		name: "Ms. Mestas",
+		aboutMe:
+			"I have been trainig in the martial arts for over 10 years. I am a 2nd Degree Black Belt working towards my next rank. I love spending time with my familiy and love teaching karate to kids and adults. I graduate High School in 2022 and plan on doing some traveling once I graduate. Some places that i would like to see are: Japan, Hawaii, and Italy.",
+		email: "jessiemessie2003@gmail.com",
+		position: "Assistant Instructor",
+		calendly: "",
+	},
+	{
+		id: "6",
+		name: "Ms. Graham",
+		aboutMe:
+			"I am a 1st Degree Black Belt at EMA. I have been training for a little over 5 years. I recently just got married to my high school sweet heart! I love the outdoors! I like to hike, swim, ski, and snowboard.",
+		email: "doritybee@gmail.com",
+		position: "Assistant Instructor",
+		calendly: "https://calendly.com/grahambee",
+	},
+];
